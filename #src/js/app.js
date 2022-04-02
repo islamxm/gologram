21a8c2aa289746a9b5a6a0e6b89ab4a0a262f581
Islam <isxmad@gmail.com>
2022-04-02 20:03:41 +0300

    fix preloader

--- #src/js/app.js
+++ #src/js/app.js
@@ -2,12 +2,12 @@ import { authSlider } from './modules/authSlider.js';
 import * as flsFuncs from './modules/functions.js';
 import { preloader } from './modules/preloader.js';
 
-
+preloader();
 
 document.addEventListener('DOMContentLoaded', () => {
     flsFuncs.isWebp();
 
-    preloader();
+    
     authSlider();
     
 })
