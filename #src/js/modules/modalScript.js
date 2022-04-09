5fa5c47c5b64e9178db6a3d4bd4de92ec123486f
Islam <isxmadgmail.com>
2022-04-09 08:18:57 +0300

    fix post

--- #src/js/modules/modalScript.js
+++ #src/js/modules/modalScript.js
@@ -8,5 +8,34 @@ export const modalScript = () => {
         group: 'posts',
         loop: false
     });
+
+    // $(".post-settings").iziModal({
+
+    //     // group: 'posts',
+    //     loop: false
+    // });
     
+
+    const setMenu = document.querySelector('.post-settings');
+    const closeBtn = document.querySelector('.postSettingsClose');
+    const triggers = document.querySelectorAll('.postSettingsOpen');
+    if(triggers.length > 0) {
+        triggers.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                setMenu.classList.add('active');
+            });
+        });
+    }
+    if(closeBtn) {
+        closeBtn.addEventListener('click', () => {
+            setMenu.classList.remove('active');
+        });
+    }
+    if(setMenu) {
+        setMenu.addEventListener('click', (e) => {
+            if(e.target.classList.contains('post-settings')) {
+                setMenu.classList.remove('active');
+            }
+        });
+    }
 }
