c6b8fe4601064df8f92a93329ba2e67696ad8264
Islam <isxmadgmail.com>
2022-04-14 20:28:56 +0300

    post-page

--- #src/js/modules/createMenu.js
+++ #src/js/modules/createMenu.js
@@ -2,18 +2,23 @@ export const createMenu = () => {
     const btn = document.querySelector('.profile-self-header__action_add');
     const menu = document.querySelector('.create-menu');
     const drag = document.querySelector('.create-menu__drag_el');
+    if(btn) {
+        btn.addEventListener('click', (e) => {
+            menu.classList.add('active');
+        });
+    }
+    
+    if(menu) {
+        menu.addEventListener('click', (e) => {
+            if(e.target.classList.contains('create-menu__layer')) {
+                menu.classList.remove('active');
+            }
+        });
+    }
 
-    btn.addEventListener('click', (e) => {
-        menu.classList.add('active');
-    });
-
-    menu.addEventListener('click', (e) => {
-        if(e.target.classList.contains('create-menu__layer')) {
+    if(drag) {
+        drag.addEventListener('click', (e) => {
             menu.classList.remove('active');
-        }
-    });
-
-    drag.addEventListener('click', (e) => {
-        menu.classList.remove('active');
-    });
+        });
+    }
 };
