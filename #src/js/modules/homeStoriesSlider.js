0a6a48218d854bc46a123f0e3109499e6a50f59d
Islam <isxmadgmail.com>
2022-04-28 06:38:36 +0300

    home adaptive

--- #src/js/modules/homeStoriesSlider.js
+++ #src/js/modules/homeStoriesSlider.js
@@ -4,8 +4,18 @@ export const homeStoriesSlider = () => {
     const el = document.querySelector('.home__main_stories_slider');
     if(el) {
         const slider = new Swiper(el, {
-            slidesPerView: 7,
-            spaceBetween: 23,
+            slidesPerView: 4,
+            spaceBetween: 8,
+            breakpoints: {
+                760: {
+                    slidesPerView: 7,
+                    spaceBetween: 23,
+                },
+                520: {
+                    slidesPerView: 5,
+                    spaceBetween: 23,
+                }
+            }
         });
     }
 }
